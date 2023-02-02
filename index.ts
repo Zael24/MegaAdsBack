0293e3a6e96b6cfc81c09f3a9c84314a90472cfd
Zael24 <matuszek.rafal99@gmail.com>
2023-02-02 23:50:11 +0100

    Added error handling.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import cors from "cors";
 import "express-async-errors";
+import { ValidationError, handleErrors } from "./utils/errors";
 
 
 const app = express();
@@ -9,6 +10,12 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(json());
+
+app.get('/', async (req, res) => {
+    throw new ValidationError('Oh damn my guy!');
+});
+
+app.use(handleErrors);
 
 app.listen(3001, "0.0.0.0", () => {
     console.log('Listening on port http://localhost:3001');
