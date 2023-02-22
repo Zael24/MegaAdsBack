0d263d1a801fb2bf3bfcba3c14c8695b14782270
Zael24 <matuszek.rafal99@gmail.com>
2023-02-22 01:45:11 +0100

    Added rate-limiter.

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import cors from "cors";
 import "express-async-errors";
 import { ValidationError, handleErrors } from "./utils/errors";
+import { rateLimit } from "express-rate-limit";
 
 
 const app = express();
@@ -10,10 +11,14 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(json());
+app.use(rateLimit({
+     windowMs: 15 * 60 * 1000, // 15 minutes
+     max: 100, //Limit each IP to 100 requests per `window` 
+}));
 
-app.get('/', async (req, res) => {
-    throw new ValidationError('Oh damn my guy!');
-});
+// app.get('/', async (req, res) => {
+//     throw new ValidationError('Oh damn my guy!');
+// });
 
 app.use(handleErrors);
 
